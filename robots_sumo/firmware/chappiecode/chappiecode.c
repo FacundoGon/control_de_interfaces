104ca5e5880f578cd11e0760fb2cd254e234edda
Facundo Gonzalez <githubproyects@gmail.com>
2026-07-13 14:46:41 -0300

    modifique el codigo de movimiento de los motores

--- robots_sumo/firmware/chappiecode/chappiecode.c
+++ robots_sumo/firmware/chappiecode/chappiecode.c
@@ -1,49 +1,30 @@
 #include <stdio.h>
 #include "pico/stdlib.h"
-#include "pico/cyw43_arch.h"
 
 // definir constantes con los pines/puertos que irán conectados al puente h
+#define ENA1 1
+#define ENA2 2
+#define ENA3 3
+#define ENA4 4
 
-//#define ENA 7
+int main(void) {
+  // Inicializo el USB
+  stdio_init_all();
+  // Demora para esperar la conexion
+  sleep_ms(1000);
 
+  // Initializing and setting pins
+    gpio_init(ENA1);
+    gpio_init(ENA2);
+    gpio_init(ENA3);
+    gpio_init(ENA4);
 
-int main()
-{
-    stdio_init_all();
+    gpio_set_dir(ENA1, GPIO_OUT);
+    gpio_set_dir(ENA2, GPIO_OUT);
+    gpio_set_dir(ENA3, GPIO_OUT);
+    gpio_set_dir(ENA4, GPIO_OUT);
 
-    // Initializing and setting pins
-    gpio_init(1);
-    gpio_init(2);
-    gpio_init(3);
-    gpio_init(4);
-
-    gpio_set_dir(1, GPIO_OUT);
-    gpio_set_dir(2, GPIO_OUT);
-    gpio_set_dir(3, GPIO_OUT);
-    gpio_set_dir(4, GPIO_OUT);
-
-    /*
-    // Initialise the Wi-Fi chip
-    if (cyw43_arch_init()) {
-        printf("Wi-Fi init failed\n");
-        return -1;
-    }
-
-    // Enable wifi station
-    cyw43_arch_enable_sta_mode();
-
-    printf("Connecting to Wi-Fi...\n");
-    if (cyw43_arch_wifi_connect_timeout_ms("Your Wi-Fi SSID", "Your Wi-Fi Password", CYW43_AUTH_WPA2_AES_PSK, 30000)) {
-        printf("failed to connect.\n");
-        return 1;
-    } else {
-        printf("Connected.\n");
-        // Read the ip address in a human readable way
-        uint8_t *ip_address = (uint8_t*)&(cyw43_state.netif[0].ip_addr.addr);
-        printf("IP address %d.%d.%d.%d\n", ip_address[0], ip_address[1], ip_address[2], ip_address[3]);
-    }
-*/
-    while (true) {
+  while (true) {
         // Go Forward
         if (true){
             gpio_put(1, 0);
@@ -79,5 +60,6 @@ int main()
             gpio_put(3, 1);
             gpio_put(4, 0);
         }
-    }
+  }
+  return 0;
 }
