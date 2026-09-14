80e249f3abb93ca0b6a0cbe3d77998033eee96be
avionica <facundongonzalez@gmail.com>
2026-09-14 16:53:22 -0300

    Añadi funciones para que sea mas legible el codigo y puse los PWM

--- robots_sumo/firmware/chappiecode/chappiecode.c
+++ robots_sumo/firmware/chappiecode/chappiecode.c
@@ -1,11 +1,50 @@
 #include <stdio.h>
 #include "pico/stdlib.h"
+#include "hardware/pwm.h"
+
 
 // definir constantes con los pines/puertos que irán conectados al puente h
 #define ENA1 1
 #define ENA2 2
 #define ENA3 3
 #define ENA4 4
+#define PWMA 6
+#define PWMB 7
+
+
+// Funciones
+
+void Avanzar() {
+    gpio_put(ENA1, 0);
+    gpio_put(ENA2, 1);
+
+    gpio_put(ENA3, 0);
+    gpio_put(ENA4, 1);
+}
+void Retroceder() {
+    gpio_put(ENA1, 1);
+    gpio_put(ENA2, 0);
+
+    gpio_put(ENA3, 1);
+    gpio_put(ENA4, 0);
+}
+void Girar_Derecha() {
+    gpio_put(ENA1, 1);
+    gpio_put(ENA2, 0);
+
+    gpio_put(ENA3, 0);
+    gpio_put(ENA4, 1);
+}
+void Girar_Izquierda() {
+    gpio_put(ENA1, 0);
+    gpio_put(ENA2, 1);
+
+    gpio_put(ENA3, 1);
+    gpio_put(ENA4, 0);
+}
+void Frenar() {
+
+}
 
 int main(void) {
   // Inicializo el USB
@@ -18,47 +57,38 @@ int main(void) {
     gpio_init(ENA2);
     gpio_init(ENA3);
     gpio_init(ENA4);
+    gpio_init(PWMA);
+    gpio_init(PWMB);
 
     gpio_set_dir(ENA1, GPIO_OUT);
     gpio_set_dir(ENA2, GPIO_OUT);
     gpio_set_dir(ENA3, GPIO_OUT);
     gpio_set_dir(ENA4, GPIO_OUT);
+    gpio_set_function(PWMA, GPIO_FUNC_PWM);
+    uint slice_num = pwm_gpio_to_slice_num(PWMA);
+    gpio_set_function(PWMB, GPIO_FUNC_PWM);
+    uint slice_num = pwm_gpio_to_slice_num(PWMB);
+    
 
   while (true) {
         // Go Forward
         if (true){
-            gpio_put(ENA1, 0);
-            gpio_put(ENA2, 1);
-
-            gpio_put(ENA3, 0);
-            gpio_put(ENA4, 1);
+            Avanzar();
         }
 
         // Turn Right
         if (false){
-            gpio_put(ENA1, 1);
-            gpio_put(ENA2, 0);
-
-            gpio_put(ENA3, 0);
-            gpio_put(ENA4, 1);
+            Girar_Derecha();
         }
 
         // Turn Left
         if (false){
-            gpio_put(ENA1, 0);
-            gpio_put(ENA2, 1);
-
-            gpio_put(ENA3, 1);
-            gpio_put(ENA4, 0);
+            Girar_Izquierda();
         }
 
         // Go Backward
         if (false){
-            gpio_put(ENA1, 1);
-            gpio_put(ENA2, 0);
-
-            gpio_put(ENA3, 1);
-            gpio_put(ENA4, 0);
+            Retroceder();
         }
   }
   return 0;
